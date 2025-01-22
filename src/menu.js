07a6d08f0bbf5dc5cfcd25cbf6d45fa16eb2073f
corythchambers <corythchambers@gmail.com>
2025-01-22 07:35:02 -0800

    restaurant

---
+++ src/menu.js
@@ -0,0 +1,10 @@
+export function menuContent() {
+    document.getElementById('content').innerHTML = `
+            <h1>This is the menu</h1>
+        <ul>
+            <li>like I said we serve flowers</li>
+            <li>more flowers</li>
+            <li>flowers</li>
+        </ul>
+    `
+}
