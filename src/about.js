584f0db7f5f3a3fa5b0046716f2fab83c910c19a
corythchambers <corythchambers@gmail.com>
2025-01-22 07:48:01 -0800

    nuke em

--- src/about.js
+++
@@ -1,7 +0,0 @@
-export function aboutContent() {
-    document.getElementById('content').innerHTML = `
-        <h1>About Cory's Restaurant</h1>
-        <p>Oh it's so good it'll make your head spin</p>
-        <p>We have flowers and edible flowers, but we won't tell you which is which</p>
-    `;
-}
